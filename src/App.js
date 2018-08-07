e81f97730afc56c6d0510df6724a857dbffc1b37
Davis <davis.sermons@gmail.com>
2018-08-07 19:24:28 +0300

    feat: fetching data from foursquare

--- src/App.js
+++ src/App.js
@@ -7,24 +7,48 @@ import Map from './components/Map.js'
 class App extends Component {
   state = {
     locations: [],
+    showLocations: [],
 
   }
 
+  componentDidMount() {
+    this.getLocations()
+  }
+
+  // Creating locations array by fetching data from foursquare API
   getLocations = () => {
     fetch('https://api.foursquare.com/v2/venues/search?ll=56.946285,24.105078&intent=browse&radius=2000&categoryId=4bf58dd8d48988d163941735&limit=15&client_id=DNN2KCYLYXI3WC1AOCN2SH5RY2ALMLHO5LNLFEUASSYQWO0F&client_secret=3WGFL1JW4OABW3403BED4ZPYWJM1LDYRL4PAERKADSRLR2FP&v=20180805')
     .then( response => response.json())
-    .then (data => {
-      this.setState({ locations: data.response.venues})
+    .then (data => data.response.venues.map(place => (
+      {
+        id: place.id,
+        name: place.name,
+        lat: place.location.lat,
+        lng: place.location.lng,
+        address: place.location.formattedAddress
+      }
+    ))).then(locations => {
+      this.setState({ showLocations: locations, locations:locations})
     }).catch(err => console.log(err))
   }
 
   render() {
+    console.log(this.state.locations)
+    const {
+      locations,
+      showlocations
+    } = this.state
+
     return (
       <div className="App">
         <NavigationPanel/>
         <div className="Map-container">
-          <SearchPanel/>
-          <Map/>
+          <SearchPanel
+            locations={locations}
+          />
+          <Map
+            locations={locations}
+          />
         </div>
       </div>
     );
