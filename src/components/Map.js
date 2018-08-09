bf76404ff78957f013fdc6bdf08bbe2242bc1ac7
Davis <davis.sermons@gmail.com>
2018-08-09 22:51:58 +0300

    feat: markers appear on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import { compose, withProps } from "recompose"; //Using "recompose" to simplify component - described in https://tomchentw.github.io/react-google-maps/#usage--configuration
 import { withGoogleMap, GoogleMap, withScriptjs, Marker, InfoWindow } from 'react-google-maps'; //Using "react-google-maps" components from https://tomchentw.github.io/react-google-maps/
+import { compose, withProps, withStateHandlers } from 'recompose'
+import { FaTree } from "react-icons/fa";
 
+export default class Map extends Component {
 
-class Map extends Component {
    render() {
-   const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter = { { lat: 56.946285, lng: 24.105078 } }
-        defaultZoom = { 14 }
+     const locations = this.props.locations.map(place => {
+      const location = {
+        position: {
+          lat: place.lat,
+          lng: place.lng
+        },
+      }
+      return (
+      <Marker key={place.id} {...location}>
+        <InfoWindow>
+          <FaTree/>
+        </InfoWindow>
+      </Marker>
+    )
+  })
+
+    const ProjectMap = compose(withStateHandlers(() => ({
+      isOpen: false,
+    }), {
+      onToggleOpen: ({ isOpen }) => () =>
+      ({isOpen: !isOpen,})
+    }),
+    withScriptjs,
+    withGoogleMap
+    )(props =>
+    <GoogleMap
+      defaultCenter = { { lat: 56.9514, lng: 24.1111 } }
+      defaultZoom = { 14 }
+      options = {{  mapTypeControl: false }}
       >
-      </GoogleMap>
-   ));
+     <Marker onClick={props.onToggleOpen}>
+       {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+         <FaTree />
+       </InfoWindow>}
+     </Marker>
+     {props.isMarkerShown && locations } />
+   </GoogleMap>
+   )
+
    return(
       <div>
-        <GoogleMapExample
-          loadingElement={<div style={{height: '100%'}}/>}
+        <ProjectMap
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCdigiQdWVM8WPznyVBAmGuvAFOr7TBjMc&v=3.exp&libraries=geometry,drawing,place"
+          loadingElement={<div style={{height: '100%'}}/>}
           containerElement={ <div style={{ height: `1000px`, width: '100%' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
+          isMarkerShown
         />
       </div>
-  );
- }
-};
-
-export default Map;
+    )
+  }
+}
